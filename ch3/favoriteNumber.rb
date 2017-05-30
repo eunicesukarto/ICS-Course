c810d3a308ddf2da98c2c40226f243e286fe2e23
eunicesukarto <eunicesukarto@gmail.com>
2017-05-30 21:28:32 +0700

    Add files via upload

---
+++ ch3/favoriteNumber.rb
@@ -0,0 +1,12 @@
+puts 'What\'s your favourite number?'
+num=gets.to_i
+puts (num+1).to_s+' is a bigger, better favourite number.'
+
+
+
+
+
+#Bigger, better favorite number. Write a program that asks for a person’s
+#favorite number. Have your program add 1 to the number, and then
+#suggest the result as a bigger and better favorite number. (Do be tactful
+#about it, though.)
