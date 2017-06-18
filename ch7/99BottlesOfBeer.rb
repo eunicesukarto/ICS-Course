55627778a8dd0d18341ac080cbdb194495dc5bb4
eunicesukarto <eunicesukarto@gmail.com>
2017-06-19 00:25:15 +0700

    Update 99BottlesOfBeer.rb

--- ch7/99BottlesOfBeer.rb
+++ ch7/99BottlesOfBeer.rb
@@ -7,14 +7,14 @@ while true
 	n=n-1
 	puts s3 + n.to_s + s1 + '.'
 	puts
-	if n==1
+	if n==2
 	    break
 	end
 end
+puts '2 bottles of beer on the wall, 2 bottles of beer.'
+puts 'Take one down and pass it around, 1 more bottle of beer on the wall.'
 puts '1 bottle of beer on the wall, 1 bottle of beer.'
 puts 'Take one down and pass it around, no more bottles of beer on the wall.'
 puts
 puts 'No more bottles of beer on the wall, no more bottles of beer.'
 puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
-
-
