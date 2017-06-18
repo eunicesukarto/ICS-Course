ea69820b1c5d0031abdc0e608b89944377a6976e
eunicesukarto <eunicesukarto@gmail.com>
2017-06-19 00:27:41 +0700

    Create sort.rb

---
+++ ch10/sort.rb
@@ -0,0 +1,24 @@
+def sort_wrapper words
+	sort_words words, []
+end
+
+def sort_words unsorted, sorted
+	unsorted1 = []
+	min = unsorted[0]
+	i=1
+	while i<=unsorted.length-1
+		if min>unsorted[i]
+			unsorted1.push min
+			min = unsorted[i]
+		else
+			unsorted1.push unsorted[i]
+		end
+		i=i+1
+	end
+	sorted.push min
+	if unsorted1 == []
+		return sorted
+	else
+		sort_words unsorted1, sorted
+	end
+end
