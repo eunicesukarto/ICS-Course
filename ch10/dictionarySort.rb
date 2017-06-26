f0daf2377278da567928d8551de9836187d1bc20
eunicesukarto <eunicesukarto@gmail.com>
2017-06-26 21:02:41 +0700

    Update dictionarySort.rb

--- ch10/dictionarySort.rb
+++ ch10/dictionarySort.rb
@@ -3,27 +3,28 @@ def sort_wrapper words
 end
 
 def sort_words unsorted, sorted
-	unsorted1 = []
-	min = unsorted[0]
-	i=1
-	while i<=unsorted.length-1
-		if min.downcase>unsorted[i].downcase
-			unsorted1.push min
-			min = unsorted[i]
-		else
-			unsorted1.push unsorted[i]
-		end
-		i=i+1
-	end
-	sorted.push min
-	if unsorted1 == []
+	if unsorted == []
 		return sorted
-	else
-		sort_words unsorted1, sorted
+	else 
+		min = unsorted[0]
+		min_index = 0
+		i=1
+		while i<=unsorted.length-1
+			if min.downcase>unsorted[i].downcase
+				min = unsorted[i]
+				min_index = i
+			end
+			i=i+1
+		end
+		unsorted[min_index] = unsorted[unsorted.length-1]
+		unsorted[unsorted.length-1] = min
+		unsorted.pop
+		sorted.push min
+		sort_words unsorted, sorted
 	end
 end
 
-puts 'type in as many words as you want (one word per line, continuing until you just press Enter on an empty line)'
+puts 'Type in as many words as you want (one word per line, continuing until you just press Enter on an empty line)'
 words = []
 while true
 	word = gets.chomp
